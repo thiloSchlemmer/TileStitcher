c70a7e8b89120508e9da36998083141424fd032e
thiloSchlemmer <thilo.schlemmer@gmail.com>
2018-05-25 20:26:49 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TileStitcher',
+    version='0.6',
+    packages=['tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
